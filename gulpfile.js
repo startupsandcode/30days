1a38af277c5752cc0e6a4b918de83ae5861758ae
John Mann <jmmann@ad.hugeinc.com>
2017-07-19 11:45:20 -0400

    Reorganized code base using Sass and Gulp builds

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+// Sass configuration
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    dest = './public/';
+
+gulp.task('sass', function () {
+    return gulp.src('./src/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+    return gulp.src('./src/**/*.html')
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());
+});
+
+gulp.task('images', function () {
+    return gulp.src('./src/**/images/*.*')
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('./src/**/scripts/*.*')
+        .pipe(gulp.dest(dest))
+        .pipe(connect.reload());
+});
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'public',
+        livereload: true
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['./src/**/*.scss'], ['sass']);
+    gulp.watch(['./src/**/scripts/*.js'], ['scripts']);
+    gulp.watch(['./src/**/*.html'], ['html']);
+});
+
+gulp.task('default', ['sass', 'html', 'scripts', 'images', 'connect', 'watch']);
