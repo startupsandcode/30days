fac15cd24c8d00f465fdefb3c29775503710ae9f
John Mann <me@johnmann.org>
2017-07-31 16:16:22 -0400

    sticky nav updates

--- src/blog/scripts/main.js
+++ src/blog/scripts/main.js
@@ -1,6 +1,13 @@
-/* place holder */
+
+var launchTile = function(e){
+    $('.flip-container').toggleClass('hide');
+    $('#' + this.id).toggleClass('current');
+}
+
 $(document).ready(function(){
     $("#burger-nav").on('click',function(){
         $('header nav ul').toggleClass('open');
-    })
+    });
+    $('.flip-container').on('click',launchTile);
 })
+
