7bc66532dd3853490409ee91171e682c71f4be7a
John Mann <me@johnmann.org>
2017-08-02 10:52:41 -0400

    updated build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     connect = require('gulp-connect'),
+    rimraf = require('gulp-rimraf'),
     src = './src/'
     dest = './public/';
 
@@ -52,5 +53,17 @@ gulp.task('watch', function () {
     gulp.watch([src + '**/scripts/*.js'], ['scripts']);
     gulp.watch([src + '**/*.html'], ['html']);
 });
+
+gulp.task('clean', function(){
+    console.log('deleting public');
+    return gulp.src('./public', { read: false })
+   .pipe(rimraf());
+})
+
+gulp.task('build',['clean'],function(){
+    return gulp.start('local');
+})
+
+gulp.task('local', ['sass', 'html', 'scripts', 'fonts', 'images']);
 
 gulp.task('default', ['sass', 'html', 'scripts', 'fonts', 'images', 'connect', 'watch']);
