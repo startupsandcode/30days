f9de6d2bf987db0fb1a1cdb5cccbda7f1ba4a778
John Mann <me@johnmann.org>
2017-08-07 13:42:34 -0400

    added myscroll

--- src/sandbox/myscroll/scripts/main.js
+++ src/sandbox/myscroll/scripts/main.js
@@ -57,5 +57,116 @@ window.addEventListener('load', function () {
                 }
             ]
         });
-
+    new Scrollify('#sticky')
+        .addScene({
+            trigger: '#trigger',
+            start: 0.2,
+            duration: '300px',
+            effects: [{
+                fn: Scrollify.fx.rotate,
+                options: {
+                    rad: Math.PI * 2
+                }
+            }]
+        });
+    new Scrollify('#transformer-4')
+        .addScene({
+            start: 0.3,
+            duration: '500px',
+            easing: Scrollify.easings.easeInElastic,
+            effects: [{
+                fn: Scrollify.fx.scale,
+                options: {
+                    to: 1.5,
+                    from: 1.0
+                }
+            }]
+        });
+    new Scrollify('#transformer-5')
+        .addScene({
+            start: 0.4,
+            effects: [{
+                fn: Scrollify.fx.scale,
+                options: {
+                    to: 0.5,
+                    from: 1.0
+                },
+            }, {
+                fn: Scrollify.fx.rotate,
+                options: {
+                    rad: Math.PI
+                }
+            }, {
+                fn: Scrollify.fx.translateX,
+                options: {
+                    to: 400,
+                    from: 0
+                }
+            }]
+        });
+    new Scrollify('#sticky')
+        .addScene({
+            trigger: '#multiple',
+            duration: '300px',
+            easing: Scrollify.easings.easeIn,
+            effects: [{
+                fn: Scrollify.fx.translateY,
+                options: {
+                    to: 300,
+                    from: 0 // TODO: getComputedStyle instead
+                }
+            }, {
+                fn: Scrollify.fx.fade,
+                options: {
+                    to: 0.2,
+                    from: 1
+                }
+            }, {
+                fn: Scrollify.fx.rotate,
+                options: {
+                    rad: Math.PI * 4
+                }
+            }]
+        })
+        .addScene({
+            trigger: '#multiple',
+            start: 0.7,
+            duration: '300px',
+            effects: [{
+                fn: Scrollify.fx.translateY,
+                options: {
+                    to: 0, // TODO -=, or skip from
+                    from: 300 // TODO: getComputedStyle instead
+                }
+            }, {
+                fn: Scrollify.fx.fade,
+                options: {
+                    to: 1,
+                    from: 0.2
+                }
+            }]
+        });
+    new Scrollify('#parallax').addEffect(Scrollify.fx.parallax, {
+        range: 300
+    });
+    new Scrollify('#stick') // the element to target
+        .addScene({ // start / stop, duration etc. of the effect
+            'start': 0.5, //   'start': when at 50% of the viewport
+            'duration': '500px', //   'duration': 300 pixels
+            'effects': [{
+                'fn': Scrollify.fx.stick // the effect to apply, and when (within the progress of the scene)
+            }]
+        });
+    new Scrollify('#transformer-8')
+        .addScene({
+            'applyTransform': false,
+            'effects': [{
+                'fn': Scrollify.fx.toggle,
+                'options': {
+                    0.2: 'red',
+                    0.5: 'blue',
+                    0.7: 'green'
+                }
+            }]
+        });
 });
